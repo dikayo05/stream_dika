322073e798573714dd3d81c331096d068455cf0a
Dika yonanda putra <yonandaputra05@gmail.com>
2025-04-09 14:20:39 +0700

    P2: Jawaban Soal 6

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ tambah 5 warna
 ### Soal 5
 - await for akan akan melakukan looping dan menunggu sampai stream selesai sebelum lanjut ke kode berikutnya
 - listen berjalan di background jadi tidak perlu menunggu selesai untuk menjalankan kode berikutnya
+
+## Praktikum 2
+### Soal 6
+- langkah 8 menginisialisasi variabel numberStream, numberStreamController, stream, dan membuat stream.listen yang di dalamnya akan mengisi variabel lastNumber
+- langkah 10 membuat method untuk membuat angka acak dari 0 - 9(totalnya 10 angka), dan menambahkannya ke variabel numberStream
+<img src="images/p2s6.gif">
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:stream_dika/color_stream.dart';
+import 'package:stream_dika/stream.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -28,6 +30,9 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
+  int lastNumber = 0;
+  late StreamController numberStreamController;
+  late NumberStream numberStream;
 
   void changeColor() async {
     // await for (var eventColor in colorStream.getColors()) {
@@ -43,21 +48,51 @@ class _StreamHomePageState extends State<StreamHomePage> {
     });
   }
 
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
+  }
+
   @override
   void initState() {
     super.initState();
-    colorStream = ColorStream();
-    changeColor();
+    // colorStream = ColorStream();
+    // changeColor();
+
+    numberStream = NumberStream();
+    numberStreamController = numberStream.controller;
+    Stream stream = numberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    numberStreamController.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stream - Dika yonanda putra'),
-      ),
-      body: Container(
-        decoration: BoxDecoration(color: bgColor),
+      appBar: AppBar(title: const Text('Stream - Dika yonanda putra')),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(lastNumber.toString()),
+            ElevatedButton(
+              onPressed: () => addRandomNumber(),
+              child: const Text('New Random Number'),
+            ),
+          ],
+        ),
       ),
     );
   }
